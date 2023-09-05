6dd065a58976df8c213165b207e07206298783c0
Meghana Hemkant Sonawane <131383737+123megh@users.noreply.github.com>
2023-09-05 10:53:10 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -7,27 +7,26 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 
         if (targetElement) {
             window.scrollTo({
-                top: targetElement.offsetTop - 50, // Adjust the scroll offset as needed
+                top: targetElement.offsetTop - 50,
                 behavior: 'smooth'
             });
         }
     });
 });
 
-// Form submission handling (you can customize this part)
+
 document.querySelector('.contact-form').addEventListener('submit', function(e) {
     e.preventDefault();
 
     const formData = new FormData(this);
 
-    // Example: Log form data to the console
+    
     for (const pair of formData.entries()) {
         console.log(pair[0] + ': ' + pair[1]);
     }
 
-    // You can add your own code here to handle form submission,
-    // such as sending an email or saving data to a database.
+    
 
-    // Clear form inputs after submission
+    
     this.reset();
 });
